3e2641bcac12e9984d9f740229412c48dd5cf561
DiscreteTom <discrete_tom@outlook.com>
2021-12-21 09:33:51 +0800

    add ping-is-not-working page

--- src/renderer/pages/index.vue
+++ src/renderer/pages/index.vue
@@ -1,5 +1,21 @@
 <template>
-  <div></div>
+  <div>
+    <v-expansion-panels>
+      <v-expansion-panel>
+        <v-expansion-panel-header> EC2 </v-expansion-panel-header>
+        <v-expansion-panel-content>
+          <v-list nav dense>
+            <v-list-item to="/ec2/ping">
+              <v-list-item-icon>
+                <v-icon> mdi-link </v-icon>
+              </v-list-item-icon>
+              <v-list-item-title> Ping is not working </v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </div>
 </template>
 
 <script>
