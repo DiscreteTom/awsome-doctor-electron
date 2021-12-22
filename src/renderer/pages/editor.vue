422c26af6ec3ec9b38701f97a93c6d9d6633085b
DiscreteTom <discrete_tom@outlook.com>
2021-12-22 09:42:09 +0800

    optimize editor page UI

--- src/renderer/pages/editor.vue
+++ src/renderer/pages/editor.vue
@@ -1,29 +1,46 @@
 <template>
   <div>
-    <h2>Create a new workflow:</h2>
+    <v-btn @click="test" color="success">Test Workflow</v-btn>
+    <v-btn @click="exportFile" class="ml-3">Export to YAML</v-btn>
+    <v-btn @click="exportFile" class="ml-3">Load from YAML</v-btn>
+    <div class="d-flex align-center mt-3">
+      <h2>Create a new workflow:</h2>
+      <!-- title -->
+      <v-text-field
+        label="Workflow Title"
+        placeholder="Ping is not working."
+        v-model="title"
+        hide-details
+        class="ml-3"
+        outlined
+        dense
+      />
+    </div>
 
-    <v-divider />
-
-    <!-- title -->
-    <v-text-field
-      label="Workflow Title"
-      placeholder="Ping is not working."
-      v-model="title"
-    />
-
-    <v-divider />
+    <v-divider class="my-2" />
 
     <!-- data -->
-    <h3>Data</h3>
+    <div class="d-flex align-center">
+      <h3>Data</h3>
+      <v-btn @click="addData" icon class="ml-3">
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+    </div>
+
     <div v-if="workflowData.length">
-      <v-row v-for="(data, i) in workflowData" :key="i">
-        <v-col>
-          <v-text-field label="Key" v-model="data.key" />
+      <v-row
+        v-for="(data, i) in workflowData"
+        :key="i"
+        class="align-center"
+        dense
+      >
+        <v-col cols="3">
+          <v-text-field label="Key" v-model="data.key" hide-details />
         </v-col>
         <v-col>
-          <v-text-field label="Value" v-model="data.value" />
+          <v-text-field label="Value" v-model="data.value" hide-details />
         </v-col>
-        <v-col>
+        <v-col cols="1">
           <v-btn icon @click="removeData(i)">
             <v-icon>mdi-close</v-icon>
           </v-btn>
@@ -31,26 +48,34 @@
       </v-row>
     </div>
     <div v-else>
-      <v-alert type="info"> No data.</v-alert>
+      <v-alert type="info" outlined dense> No data.</v-alert>
     </div>
-    <v-btn @click="addData">Add Data</v-btn>
 
-    <v-divider />
+    <v-divider class="my-2" />
 
     <!-- inputs -->
-    <h3>Inputs</h3>
+    <div class="d-flex align-center">
+      <h3>Inputs</h3>
+      <v-btn @click="addInput" icon class="ml-3">
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+    </div>
     <div v-if="inputs.length">
-      <v-row v-for="(input, i) in inputs" :key="i">
+      <v-row v-for="(input, i) in inputs" :key="i" class="align-center" dense>
         <v-col>
-          <v-text-field label="Label" v-model="input.label" />
+          <v-text-field label="Label" v-model="input.label" hide-details />
         </v-col>
         <v-col>
-          <v-text-field label="Placeholder" v-model="input.placeholder" />
+          <v-text-field
+            label="Placeholder"
+            v-model="input.placeholder"
+            hide-details
+          />
         </v-col>
         <v-col>
-          <v-text-field label="Store" v-model="input.store" />
+          <v-text-field label="Store" v-model="input.store" hide-details />
         </v-col>
-        <v-col>
+        <v-col cols="1">
           <v-btn icon @click="removeInput(i)">
             <v-icon>mdi-close</v-icon>
           </v-btn>
@@ -58,23 +83,27 @@
       </v-row>
     </div>
     <div v-else>
-      <v-alert type="info"> No inputs.</v-alert>
+      <v-alert type="info" outlined dense> No inputs.</v-alert>
     </div>
-    <v-btn @click="addInput">Add Input</v-btn>
 
-    <v-divider />
+    <v-divider class="my-2" />
 
     <!-- steps -->
-    <h3>Steps</h3>
+    <div class="d-flex align-center">
+      <h3>Steps</h3>
+      <v-btn @click="addStep" icon class="ml-3">
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+    </div>
     <div v-if="steps.length">
-      <v-row v-for="(step, i) in steps" :key="i">
-        <v-col>
-          <v-text-field label="Name" v-model="step.name" />
+      <v-row v-for="(step, i) in steps" :key="i" class="align-center" dense>
+        <v-col cols="3">
+          <v-text-field label="Name" v-model="step.name" hide-details />
         </v-col>
         <v-col>
           <v-textarea label="JavaScript" v-model="step.js" />
         </v-col>
-        <v-col>
+        <v-col cols="1">
           <v-btn icon @click="removeStep(i)">
             <v-icon>mdi-close</v-icon>
           </v-btn>
@@ -82,14 +111,8 @@
       </v-row>
     </div>
     <div v-else>
-      <v-alert type="info"> No steps.</v-alert>
+      <v-alert type="info" outlined dense> No steps.</v-alert>
     </div>
-    <v-btn @click="addStep">Add Step</v-btn>
-
-    <!-- test workflow -->
-    <v-divider />
-    <v-btn @click="test">Test Workflow</v-btn>
-    <v-btn @click="exportFile">Export to YAML</v-btn>
   </div>
 </template>
 
