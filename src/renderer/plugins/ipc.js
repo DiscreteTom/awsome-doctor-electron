834fb477554ee662d80a08e1a08bafdb2ab00efd
DiscreteTom <discrete_tom@outlook.com>
2021-12-26 12:06:46 +0800

    fix ipc plugin one logic

--- src/renderer/plugins/ipc.js
+++ src/renderer/plugins/ipc.js
@@ -1,16 +1,18 @@
 import { ipcRenderer } from "electron";
 
-let channelSet = new Set();
+let channelMap = {};
 
 let ipc = {
   /**
-   * every channel can only be registered once
+   * every channel can only has one listener
    */
   one(channel, f) {
-    if (!channelSet.has(channel)) {
-      channelSet.add(channel);
-      ipcRenderer.on(channel, f);
+    if (Object.keys(channelMap).indexOf(channel) == -1) {
+      ipcRenderer.on(channel, (event, arg) => {
+        channelMap[channel](event, arg);
+      });
     }
+    channelMap[channel] = f;
   },
   send(channel, ...arg) {
     ipcRenderer.send(channel, ...arg);
