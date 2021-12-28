c66e278da9032fa52b29edd153727de50049fe86
DiscreteTom <discrete_tom@outlook.com>
2021-12-28 17:21:57 +0800

    add aws s3 & elasticloadbalancingv2

--- src/renderer/plugins/aws.js
+++ src/renderer/plugins/aws.js
@@ -1,5 +1,7 @@
+import { S3 } from "@aws-sdk/client-s3";
 import { EC2 } from "@aws-sdk/client-ec2";
 import { RDS } from "@aws-sdk/client-rds";
+import { ElasticLoadBalancingV2 } from "@aws-sdk/client-elastic-load-balancing-v2";
 
 let aws = {};
 
@@ -12,7 +14,9 @@ function configure({ accessKeyId, secretAccessKey, region }) {
     region,
   };
   aws["ec2"] = new EC2(config);
+  aws["s3"] = new S3(config);
   aws["rds"] = new RDS(config);
+  aws["elbv2"] = new ElasticLoadBalancingV2(config);
 }
 
 aws["configure"] = configure;
